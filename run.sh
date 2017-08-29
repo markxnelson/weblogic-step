f2694b6080611e1f4864f7caa61607a3039b2572
Mark Nelson <mark.x.nelson@oracle.com>
2017-08-29 18:22:59 -0400

    Update this script to try to set up the right environment variables for, and to run the script to create and start the weblogic domain.
    This is done under a sudo -u oracle since we need to login to the container as root so wercker
    can create its /pipeline etc.
    
    Also tried to run wlst and jsut connect and say hello.

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-echo "Hello WebLogic Step"
+echo "Hello from the WebLogic Step"
+sudo -u oracle DOMAIN_HOME=/u01/oracle/user_projects/domain/base-domain && \
+ADMIN_PASSWORD=welcome1 && \
+ADMIN_USERNAME=weblogic && \
+DOMAIN_NAME=base_domain && \
+/u01/oracle/createAndStartEmptyDomain.sh && \
+/u01/oracle/oracle_common/common/bin/wlst.sh << EOF
+connect ('weblogic', 'welcome1', 't3://localhost:7001')
+ls ()
+exit() 
+EOF
+
