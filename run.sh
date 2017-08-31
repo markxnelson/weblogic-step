477cda60159fb3883f2896bfe7329fa57ae3f78a
Gary McAdoo <gary.mcadoo@oracle.com>
2017-08-31 10:39:31 -0500

    updated version and put the sudo command back in to try with different container

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Hello from the WebLogic Step"
-DOMAIN_HOME=/u01/oracle/user_projects/domain/base-domain && \
+sudo -u oracle DOMAIN_HOME=/u01/oracle/user_projects/domain/base-domain && \
 ADMIN_PASSWORD=welcome1 && \
 ADMIN_USERNAME=weblogic && \
 DOMAIN_NAME=base_domain && \
