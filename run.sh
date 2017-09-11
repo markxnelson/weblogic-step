af9758ce2e61181181a0d5bd7f8def8622719c04
Mark Nelson <mark.x.nelson@oracle.com>
2017-09-11 14:25:57 -0400

    get config param

--- run.sh
+++ run.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 echo "Hello from the WebLogic Step"
-# adding a comment
-sudo -u oracle DOMAIN_HOME=/u01/oracle/user_projects/domain/base-domain && \
-ADMIN_PASSWORD=welcome1 && \
-ADMIN_USERNAME=weblogic && \
-DOMAIN_NAME=base_domain && \
-/u01/oracle/createAndStartEmptyDomain.sh && \
-/u01/oracle/oracle_common/common/bin/wlst.sh << EOF
-connect ('weblogic', 'welcome1', 't3://localhost:7001')
-ls ()
-exit() 
-EOF
+echo "Reading the config from :" $CONFIG
 
